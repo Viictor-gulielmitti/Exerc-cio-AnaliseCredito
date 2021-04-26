1e71db007aa4fe010a28caec74a0d50f54ede64b
Viictor-gulielmitti <Victorgulielmitti284@gmail.com>
2021-04-26 20:18:18 -0300

    credito

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exerc_cio_AnaliseCredito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Emprestimo duplo,  parcelas , renda , porcentagem , valorParcela; 
+
+            
+            Console . WriteLine ("Analisar crédito" );
+            
+            Console . Write ( " Digite o valor do emprestimo ... " );
+            Emprestimo = Convert.ToDouble (Console.ReadLine());
+
+            Console . Write ( " Digite a quantidade de parcelas ... " );
+            parcelas  =  Convert.ToDouble ( Console . ReadLine ());
+
+            Console . Write ( " Digite a sua Renda ... " );
+            renda  =  Convert.ToDouble ( Console . ReadLine ());
+
+            porcentagem  =  renda  /  100  *  30;
+
+            valorParcela  = ( Emprestimo /  parcelas);
+
+            if ( porcentagem  >  valorParcela )
+            {
+                Console . WriteLine ( " \n Emprestimo Autorizado " );
+            }
+            else  if ( porcentagem  <  valorParcela )
+            {
+                 Console . WriteLine ($" \n Emprestimo Não Autorizado" );
+            }
+    
+        } 
+
+    }
+}
+        
+    
+
